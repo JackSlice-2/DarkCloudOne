6fe7643dc3238037a6309531d092e289dcf320dc
JackSlice-2 <jackslice9@gmail.com>
2023-07-16 11:11:46 -0300

    Liked Songs Functyion and page done

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,15 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps {
     searchParams: {
         title: string;
     }
 };
+
+export const revalidate = 0;
 
 const Search = async ({ searchParams }: SearchProps) => {
     const songs = await getSongsByTitle(searchParams.title);
@@ -22,6 +25,7 @@ const Search = async ({ searchParams }: SearchProps) => {
                     <SearchInput />
                 </div>
             </Header>
+            <SearchContent songs={songs}/>
         </div>
     );
 }
